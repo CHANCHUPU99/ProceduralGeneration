4c0dbdd7f3e72efa5bfa6251f4f56dd15c20e34c
CHANCHUPU99 <106758212+CHANCHUPU99@users.noreply.github.com>
2024-10-29 17:06:12 -0600

    Creacion de clase TypeTile

---
+++ Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,173 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public TileTypes[,] theGrid;
+    public Tilemap tilemap;
+    public Tilemap userTilemap;
+    public Tile drawedTile;
+    public int rows = 350;
+    public int columns = 350;
+    private bool simulationIsRunning;
+    public Tile deadTile;
+
+
+    private void Start() {
+        theGrid = new TileTypes[rows, columns];
+        for (int i = 0; i < rows; i++) {
+            for (int c = 0; c < columns; c++) {
+                //esto es test
+                //theGrid[i, c] = new TileTypes(false,);
+            }
+        }
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 coursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int tilePos = userTilemap.WorldToCell(coursorPos);
+            if (tilePos.x >= 0 && tilePos.x < rows && tilePos.y >= 0 && tilePos.y < columns) {
+                updateGrid(tilePos);
+            }
+        }
+        if (simulationIsRunning) {
+            StartCoroutine(generationsInterval());
+            //runGameOfLife();
+        }
+    }
+
+    public void runGameOfLife() {
+        TileTypes[,] tempGrid = new TileTypes[rows, columns];
+        for (int i = 0; i < rows; i++) {
+            for (int c = 0; c < columns; c++) {
+                tempGrid[i, c] = new TileTypes();
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int c = 0; c < columns; c++) {
+                int currentNeighs = checkNeighCells(i, c);
+                if (theGrid[i, c].bIsAlive) {
+                    if (currentNeighs < 2 || currentNeighs > 3) {
+                        tempGrid[i, c].bIsAlive = false;
+                    } else {
+                        tempGrid[i, c].bIsAlive = true;
+                    }
+                } else {
+                    if (currentNeighs == 3) {
+                        tempGrid[i, c].bIsAlive = true;
+                    } else {
+                        tempGrid[i, c].bIsAlive = false;
+                    }
+                }
+            }
+        }
+        theGrid = tempGrid;
+        //printGridState();
+        updateVisualGrid();
+    }
+
+    private void updateGrid(Vector3Int tilePos) {
+        TileBase currentTile = tilemap.GetTile(tilePos);
+        if (currentTile == null) {
+            tilemap.SetTile(tilePos, drawedTile);
+            userTilemap.SetTile(tilePos, drawedTile);
+            theGrid[tilePos.x, tilePos.y].bIsAlive = true;
+        } else {
+            //tilemap.SetTile(tilePos, deadTile);
+            //userTilemap.SetTile(tilePos, deadTile);
+            //theGrid[tilePos.x, tilePos.y].bIsAlive = false;
+            tilemap.SetTile(tilePos, null);
+            userTilemap.SetTile(tilePos, null);
+            theGrid[tilePos.x, tilePos.y].bIsAlive = false;
+        }
+    }
+
+    //users grid
+    private void updateVisualGrid() {
+        for (int i = 0; i < rows; i++) {
+            for (int c = 0; c < columns; c++) {
+                Vector3Int currentGridPos = new Vector3Int(i, c);
+                if (theGrid[i, c].bIsAlive) {
+                    tilemap.SetTile(currentGridPos, drawedTile);
+                    userTilemap.SetTile(currentGridPos, drawedTile);
+                } else {
+                    tilemap.SetTile(currentGridPos, null);
+                    userTilemap.SetTile(currentGridPos, null);
+                }
+            }
+        }
+    }
+
+    //Funtion to delete theGrid
+    public void clearTheGrid() {
+        for (int i = 0; i < rows; i++) {
+            for (int c = 0; c < columns; c++) {
+                Vector3Int currentGridPos = new Vector3Int(i, c);
+                if (theGrid[i, c].bIsAlive) {
+                    theGrid[i, c].bIsAlive = false;
+                    tilemap.SetTile(currentGridPos, null);
+                    userTilemap.SetTile(currentGridPos, null);
+                } else {
+                    theGrid[i, c].bIsAlive = false;
+                    tilemap.SetTile(currentGridPos, null);
+                    userTilemap.SetTile(currentGridPos, null);
+                }
+            }
+        }
+    }
+
+    //claseeeeeeee
+    int checkNeighCells(int x, int y) {
+        int aliveNeighs = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                int checkX = x + i;
+                int checkY = y + j;
+                if (checkX >= 0 && checkX < rows && checkY >= 0 && checkY < columns) {
+                    if (theGrid[checkX, checkY].bIsAlive) {
+                        aliveNeighs++;
+                    }
+
+                }
+            }
+        }
+        return aliveNeighs;
+    }
+
+    IEnumerator generationsInterval() {
+        while (simulationIsRunning) {
+            yield return new WaitForSeconds(0.3f);
+            runGameOfLife();
+            //updateVisualGrid();
+        }
+    }
+
+    //buttons functions
+    public void startGameOfLife() {
+        simulationIsRunning = true;
+        StartCoroutine(generationsInterval());
+
+    }
+
+    public void stopGameOfLife() {
+        simulationIsRunning = false;
+    }
+
+    //function to print 
+    void printGridState() {
+        string gridState = "";
+        for (int i = 0; i < rows; i++) {
+            for (int c = 0; c < columns; c++) {
+                gridState += theGrid[i, c].bIsAlive ? "1 " : "0 ";
+            }
+            gridState += "\n";
+        }
+        Debug.Log($"Estado actual de theGrid:\n{gridState}");
+    }
+}
---
+++ Assets/Scripts/TileTypes.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTypes : MonoBehaviour
+{
+    public bool bIsAlive;
+    public bool bIsSafeToWalk;
+    public float walkSpeed;
+    public tileTypes tileTypesEnum;
+    public TileTypes() {
+        bIsAlive = false;
+    }
+    public TileTypes(bool _isAlive, tileTypes _objType, bool _isSafeToWalk, float _walkSpeed) {
+        bIsAlive = _isAlive;
+        tileTypesEnum = _objType;
+        bIsSafeToWalk = _isSafeToWalk;
+        walkSpeed = _walkSpeed;
+    }
+
+    public void setIsSafeToWalk(bool _isSafeToWalk) {
+        bIsSafeToWalk = _isSafeToWalk;
+    }
+
+    public bool getIsSafeToWalk() {
+        return bIsSafeToWalk;
+    }
+
+    public void setWalkSpeed(float _walkSpeed) {
+        walkSpeed = _walkSpeed;
+    }
+
+    public float getWalkSpeed() {
+        return walkSpeed;
+    }
+
+    public void setTileType(tileTypes _tileTypesEnum) {
+        tileTypesEnum = _tileTypesEnum;
+    }
+
+    public tileTypes GetTileType() {
+        return tileTypesEnum;
+    }
+
+    public enum tileTypes {
+        Grass,
+        Mud,
+        Spikes,
+        Stone,
+        Water
+    }
+}
